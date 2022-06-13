0dfafbea5545b05b600345a0f1d818854baf5e32
NishaShetty <103092545+nishashettyy@users.noreply.github.com>
2022-06-13 14:49:34 +0530

    Add files via upload

---
+++ config_server/src/test/java/com/config/terrapay/ConfigServerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.config.terrapay;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ConfigServerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
